a9ff8cb07d859ea9828e2211aa9a306308b5d8ad
Enzo Rouet <enzorouet04@gmail.com>
2026-01-14 22:37:12 +0100

    Add loop render in index.js

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 import { getCoins } from "./src/API.js";
+import { render } from "./src/render.js";
 
-getCoins();
+async function init() {
+  try {
+    const data = await getCoins();
+    console.log("Données reçues, mise à jour du tableau...");
+    render(data);
+    console.log("Données affichées !!!");
+  } catch (err) {
+    console.log("Erreur API : " + err);
+  }
+}
+
+init();
+
+setInterval(() => {
+  if (document.visibilityState === "visible") {
+    init();
+  }
+}, 30000);
+
+document.addEventListener("visibilitychange", () => {
+  if (document.visibilityState === "visible") {
+    init();
+  }
+});
