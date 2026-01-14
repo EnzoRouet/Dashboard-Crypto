6b95788a0342e7c4bd6632bb7814cb574f514700
Enzo Rouet <enzorouet04@gmail.com>
2026-01-14 22:25:56 +0100

    Render finish

--- src/render.js
+++ src/render.js
@@ -32,4 +32,37 @@ export function formatDate() {
   }).format(currentDate);
 }
 
-console.log(formatDate());
+export function render(datas) {
+  const table = document.getElementById("crypto-table-body");
+  table.innerHTML = ``;
+
+  const htmlRows = datas.map((crypto) => {
+    const css =
+      crypto.price_change_percentage_7d_in_currency > 0 ? "green" : "red";
+    const icon =
+      crypto.price_change_percentage_7d_in_currency > 0
+        ? `<i class="fa-solid fa-arrow-trend-up"></i>`
+        : `<i class="fa-solid fa-arrow-trend-down"></i>`;
+
+    return `
+      <tr>
+            <th scope="row"> <img src="${
+              crypto.image
+            }"/> <span class="crypto-name">${crypto.name}</span></th>
+            <td>${formatPrice(crypto.current_price)}</td>
+            <td class="${css}">${icon} <span>${crypto.price_change_percentage_7d_in_currency.toFixed(
+      2
+    )}%</span></td>
+            <td>
+                <div>${formatPrice(crypto.high_24h)}</div>
+                <div class="low">${formatPrice(crypto.low_24h)}</div>
+            </td>
+            <td>${crypto.ath_change_percentage.toFixed(2)}%</td>
+            <td>${formatPrice(crypto.market_cap)}</td>
+      </tr>
+    `;
+  });
+
+  table.innerHTML = htmlRows.join("");
+  document.getElementById("last-update-time").textContent = formatDate();
+}
